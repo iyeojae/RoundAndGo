3340e1d58862289ecb6aaea9275327028627145a
PokeMasterLEE <65201784+iyeojae@users.noreply.github.com>
2025-07-28 04:09:40 +0900

    Update SecurityConfig.java

--- src/main/java/org/likelionhsu/roundandgo/Security/config/SecurityConfig.java
+++ src/main/java/org/likelionhsu/roundandgo/Security/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://roundandgo.com", "https://www.roundandgo.com"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://roundandgo.com", "http://www.roundandgo.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
