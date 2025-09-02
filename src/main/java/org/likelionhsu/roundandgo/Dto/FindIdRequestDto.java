1b23d0b5cf0ebc364c228dc96384611fb973cadf
PokeMasterLEE <iyeojae@gmail.com>
2025-09-02 18:00:45 +0900

    :sparkles: 아이디 찾기 요청/응답용 DTO

---
+++ src/main/java/org/likelionhsu/roundandgo/Dto/FindIdRequestDto.java
@@ -0,0 +1,10 @@
+package org.likelionhsu.roundandgo.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FindIdRequestDto {
+    private String email;
+}
---
+++ src/main/java/org/likelionhsu/roundandgo/Dto/FindIdResponseDto.java
@@ -0,0 +1,17 @@
+package org.likelionhsu.roundandgo.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class FindIdResponseDto {
+    private List<String> foundIds; // 찾은 아이디들 (이메일들)
+    private String message;
+}
