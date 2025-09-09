a8eeb88660b06fa073634990b02d47ee73ee139a
PokeMasterLEE <iyeojae@gmail.com>
2025-09-09 20:07:49 +0900

    :recycle: 아이디 찾기 로직 삭제 및 비밀번호 변경 로직 추가

---
+++ src/main/java/org/likelionhsu/roundandgo/Dto/Request/PasswordChangeRequestDto.java
@@ -0,0 +1,15 @@
+package org.likelionhsu.roundandgo.Dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordChangeRequestDto {
+    private String email;
+    private String newPassword;
+}
---
+++ src/main/java/org/likelionhsu/roundandgo/Dto/Request/PasswordResetRequestDto.java
@@ -0,0 +1,14 @@
+package org.likelionhsu.roundandgo.Dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetRequestDto {
+    private String email;
+}
